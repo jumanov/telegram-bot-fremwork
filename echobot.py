fd1c320797b6edcb54662a0573edfc4990305374
Diyorbek <diyorbekjumanov1120@gmail.com>
2021-03-29 11:04:05 +0500

    getMe

--- echobot.py
+++ echobot.py
@@ -3,4 +3,8 @@ import os
 
 token = os.environ['token']
 echobot = telegram.Bot(token=token)
+print(echobot.getMe(), '\n\n')
 print(echobot.getMe().id)
+print(echobot.getMe().username)
+print(echobot.getMe().first_name)
+print(echobot.getMe().last_name)
