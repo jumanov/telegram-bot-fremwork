2c50b9b30e36fa6bc4055409e8479d3e3276a675
Diyorbek <diyorbekjumanov1120@gmail.com>
2021-03-29 11:12:58 +0500

    getUpdates

--- echobot.py
+++ echobot.py
@@ -1,10 +1,8 @@
 import telegram
 import os
+from pprint import pprint
 
 token = os.environ['token']
 echobot = telegram.Bot(token=token)
-print(echobot.getMe(), '\n\n')
-print(echobot.getMe().id)
-print(echobot.getMe().username)
-print(echobot.getMe().first_name)
-print(echobot.getMe().last_name)
+
+pprint(echobot.getUpdates()[0])
