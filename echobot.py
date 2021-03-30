00590079c55d41b6d24b79375e341c32d291247f
Diyorbek <diyorbekjumanov1120@gmail.com>
2021-03-30 16:57:29 +0500

    echo easy

--- echobot.py
+++ echobot.py
@@ -4,5 +4,12 @@ from pprint import pprint
 
 token = os.environ['token']
 echobot = telegram.Bot(token=token)
-
-echobot.sendMessage(1258594598, 'hi')
+update_id0 = 0
+while True:
+    data = echobot.getUpdates()[-1]
+    chat_id = data.message.chat.id
+    text = data.message.text
+    update_id1 = data.update_id
+    if update_id0 != update_id1:
+        echobot.sendMessage(chat_id, text)
+        update_id0 = update_id1
