f153d61ac5379eacc8c97cb6ab30ad13b13de01a
Diyorbek <diyorbekjumanov1120@gmail.com>
2021-03-30 17:54:33 +0500

    messageHandler

--- echobot.py
+++ echobot.py
@@ -7,9 +7,15 @@ def command(update, context):
     update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 
+def reply_Msg(update, context):
+    msg_text = update.message.text
+    update.message.reply_text(msg_text)
+
+
 updater = Updater(token=TOKEN)
 
 updater.dispatcher.add_handler(CommandHandler('start', command))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, reply_Msg))
 
 updater.start_polling()
 updater.idle()
